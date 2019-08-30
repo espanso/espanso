661d927e678fe4e44c58fd309dd1c400dd7a5629
Federico Terzi <federico-terzi@users.noreply.github.com>
2019-08-30 14:33:40 +0200

    First native windows bridge

---
+++ build.rs
@@ -0,0 +1,10 @@
+extern crate cmake;
+use cmake::Config;
+
+fn main()
+{
+    let dst = Config::new("native/libwinbridge").build();
+
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=winbridge");
+}
