e2b7940108e1fcd2891f2f010964b4b2bb62e826
Federico Terzi <federicoterzi96@gmail.com>
2019-09-07 00:38:13 +0200

    Add experimental support for clipboard backend on linux.

--- src/engine.rs
+++ src/engine.rs
@@ -1,19 +1,22 @@
 use crate::matcher::{Match, MatchReceiver};
 use crate::keyboard::KeyboardSender;
 use crate::config::Configs;
+use crate::clipboard::ClipboardManager;
+use std::sync::Arc;
 
-pub struct Engine<S> where S: KeyboardSender {
+pub struct Engine<S, C> where S: KeyboardSender, C: ClipboardManager {
     sender: S,
+    clipboard_manager: Arc<C>,
     configs: Configs,
 }
 
-impl <S> Engine<S> where S: KeyboardSender{
-    pub fn new(sender: S, configs: Configs) -> Engine<S> where S: KeyboardSender {
-        Engine{sender, configs }
+impl <S, C> Engine<S, C> where S: KeyboardSender, C: ClipboardManager{
+    pub fn new(sender: S, clipboard_manager: Arc<C>, configs: Configs) -> Engine<S, C> where S: KeyboardSender, C: ClipboardManager {
+        Engine{sender, clipboard_manager, configs }
     }
 }
 
-impl <S> MatchReceiver for Engine<S> where S: KeyboardSender{
+impl <S, C> MatchReceiver for Engine<S, C> where S: KeyboardSender, C: ClipboardManager{
     fn on_match(&self, m: &Match) {
         self.sender.delete_string(m.trigger.len() as i32);
 
@@ -21,7 +24,9 @@ impl <S> MatchReceiver for Engine<S> where S: KeyboardSender{
         // while on windows and macos, we need to emulate a Enter key press.
 
         if cfg!(target_os = "linux") {
-            self.sender.send_string(m.replace.as_str());
+            self.clipboard_manager.set_clipboard(m.replace.as_str());
+            self.sender.trigger_paste();
+            //self.sender.send_string(m.replace.as_str());
         }else{
             // To handle newlines, substitute each "\n" char with an Enter key press.
             let splits = m.replace.lines();
