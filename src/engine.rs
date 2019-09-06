fdece1249d1e1d706f946d2585a008db4ed1e39e
Federico Terzi <federicoterzi96@gmail.com>
2019-09-06 22:19:28 +0200

    Add newline support on linux. Fix #6

--- src/engine.rs
+++ src/engine.rs
@@ -17,15 +17,22 @@ impl <S> MatchReceiver for Engine<S> where S: KeyboardSender{
     fn on_match(&self, m: &Match) {
         self.sender.delete_string(m.trigger.len() as i32);
 
-        // To handle newlines, substitute each "\n" char with an Enter key press.
-        let splits = m.replace.lines();
+        // Send the expected string. On linux, newlines are managed automatically
+        // while on windows and macos, we need to emulate a Enter key press.
 
-        for (i, split) in splits.enumerate() {
-            if i > 0 {
-                self.sender.send_enter();
+        if cfg!(target_os = "linux") {
+            self.sender.send_string(m.replace.as_str());
+        }else{
+            // To handle newlines, substitute each "\n" char with an Enter key press.
+            let splits = m.replace.lines();
+
+            for (i, split) in splits.enumerate() {
+                if i > 0 {
+                    self.sender.send_enter();
+                }
+
+                self.sender.send_string(split);
             }
-
-            self.sender.send_string(split);
         }
     }
 }
