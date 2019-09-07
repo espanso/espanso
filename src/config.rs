2a4c411ca1f8ad646506ac29382a35fc55ee619b
Federico Terzi <federicoterzi96@gmail.com>
2019-09-07 16:48:55 +0200

    Add basic filtering in config

--- src/config.rs
+++ src/config.rs
@@ -9,6 +9,7 @@ use serde::{Serialize, Deserialize};
 use crate::keyboard::KeyModifier;
 use crate::system::SystemManager;
 use std::collections::HashSet;
+use regex::Regex;
 
 // TODO: add documentation link
 const DEFAULT_CONFIG_FILE_CONTENT : &str = include_str!("res/config.yaml");
@@ -18,17 +19,18 @@ const DEFAULT_CONFIG_FILE_NAME : &str = "default.yaml";
 
 // Default values for primitives
 fn default_name() -> String{ "default".to_owned() }
-fn default_toggle_interval() -> u32 {
-    230
-}
-fn default_backspace_limit() -> i32 {
-    3
-}
+fn default_filter_title() -> String{ "".to_owned() }
+fn default_toggle_interval() -> u32 { 230 }
+fn default_backspace_limit() -> i32 { 3 }
+fn default_matches() -> Vec<Match> { Vec::new() }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Configs {
     #[serde(default = "default_name")]
     pub name: String,
+
+    #[serde(default = "default_filter_title")]
+    pub filter_title: String,
 
     #[serde(default)]
     pub toggle_key: KeyModifier,
@@ -42,6 +44,7 @@ pub struct Configs {
     #[serde(default)]
     pub backend: BackendType,
 
+    #[serde(default = "default_matches")]
     pub matches: Vec<Match>
 }
 
@@ -161,37 +164,77 @@ pub trait ConfigManager {
 
 pub struct RuntimeConfigManager<S: SystemManager> {
     set: ConfigSet,
+    title_regexps: Vec<Option<Regex>>,
 
     system_manager: S
 }
 
 impl <S: SystemManager> RuntimeConfigManager<S> {
     pub fn new(set: ConfigSet, system_manager: S) -> RuntimeConfigManager<S> {
+        // Compile all the regexps
+        let title_regexps = set.specific.iter().map(
+            |config| {
+                if config.filter_title.is_empty() {
+                    None
+                }else{
+                    let res = Regex::new(&config.filter_title);
+                    if let Ok(regex) = res {
+                        Some(regex)
+                    }else{
+                        // TODO: log invalid regex error
+                        None
+                    }
+                }
+            }
+        ).collect();
+
         RuntimeConfigManager {
             set,
+            title_regexps,
             system_manager
         }
     }
 }
 
+impl <S: SystemManager> RuntimeConfigManager<S> {
+    fn active_config(&self) -> &Configs {
+        // TODO: optimize performance by avoiding some of these checks if no Configs use the filters
+
+        let active_title = self.system_manager.get_current_window_title();
+
+        if let Some(title) = active_title {
+            for (i, regex) in self.title_regexps.iter().enumerate() {
+                if let Some(regex) = regex {
+                    if regex.is_match(&title) {
+                        return &self.set.specific[i]
+                    }
+                }
+            }
+        }
+
+        // No matches, return the default mapping
+        &self.set.default
+    }
+}
+
 impl <S: SystemManager> ConfigManager for RuntimeConfigManager<S> {
     fn toggle_key(&self) -> &KeyModifier {
-        &self.set.default.toggle_key
+        &self.active_config().toggle_key
     }
 
     fn toggle_interval(&self) -> u32 {
-        self.set.default.toggle_interval
+        self.active_config().toggle_interval
     }
 
     fn backspace_limit(&self) -> i32 {
-        self.set.default.backspace_limit
+        self.active_config().backspace_limit
     }
 
     fn backend(&self) -> &BackendType {
-        &BackendType::Inject // TODO make dynamic based on system current active app
+        &self.active_config().backend
     }
 
     fn matches(&self) -> &Vec<Match> {
-        &self.set.default.matches
+        &self.active_config().matches
     }
 }
