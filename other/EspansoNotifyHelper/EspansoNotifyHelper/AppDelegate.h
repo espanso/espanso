b84418cea89a3336b66b5d3697b52bfeac1f3230
Federico Terzi <federicoterzi96@gmail.com>
2019-09-15 18:29:11 +0200

    Add GPL preamble

--- other/EspansoNotifyHelper/EspansoNotifyHelper/AppDelegate.h
+++ other/EspansoNotifyHelper/EspansoNotifyHelper/AppDelegate.h
@@ -1,10 +1,21 @@
-//
-//  AppDelegate.h
-//  NotificationHelper
-//
-//  Created by Federico on 06/09/2019.
-//  Copyright © 2019 Federico Terzi. All rights reserved.
-//
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #import <Cocoa/Cocoa.h>
 
