fb051f7d267593dfbd330ffdec0aee79eff03ad1
Federico Terzi <federicoterzi96@gmail.com>
2019-09-20 23:21:03 +0200

    First attempt with Travis CI

---
+++ ci/before_deploy.sh
@@ -0,0 +1,31 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross build --target $TARGET --release
+
+    cp target/$TARGET/release/espanso $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
