fb8018d855b7cf404e21bdde839ee9be075bc108
Federico Terzi <federico-terzi@users.noreply.github.com>
2019-09-25 20:52:21 +0200

    Add local index in package manager

---
+++ src/package/mod.rs
@@ -0,0 +1,44 @@
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pub(crate) mod default;
+use serde::{Serialize, Deserialize};
+
+pub trait PackageManager {
+    fn update_index(&self);
+    fn is_index_outdated(&self) -> bool;
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Package {
+    name: String,
+    title: String,
+    version: String,
+    repo: String,
+    desc: String,
+    author: String
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PackageIndex {
+    #[serde(rename = "lastUpdate")]
+    last_update: u64,
+
+    packages: Vec<Package>
+}
