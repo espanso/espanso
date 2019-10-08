cfbb15ac4839228bce262acd2f30da4d68a3b02e
Federico Terzi <federicoterzi96@gmail.com>
2019-10-09 00:37:42 +0200

    Moved default location for configuration and packages. Fix #73. Merge #77. Create path subcommand. Updated tests

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -35,13 +35,6 @@ pub trait Context {
     fn eventloop(&self);
 }
 
-pub fn get_data_dir() -> PathBuf {
-    let data_dir = dirs::data_local_dir().expect("Can't obtain data_dir(), terminating.");
-    let espanso_dir = data_dir.join("espanso");
-    create_dir_all(&espanso_dir).expect("Error creating espanso data directory");
-    espanso_dir
-}
-
 // MAC IMPLEMENTATION
 #[cfg(target_os = "macos")]
 pub fn new(send_channel: Sender<Event>) -> Box<dyn Context> {
@@ -58,4 +51,53 @@ pub fn new(send_channel: Sender<Event>) -> Box<dyn Context> {
 #[cfg(target_os = "windows")]
 pub fn new(send_channel: Sender<Event>) -> Box<dyn Context> {
     windows::WindowsContext::new(send_channel)
+}
+
+// espanso directories
+
+pub fn get_data_dir() -> PathBuf {
+    let data_dir = dirs::data_local_dir().expect("Can't obtain data_local_dir(), terminating.");
+    let espanso_dir = data_dir.join("espanso");
+    create_dir_all(&espanso_dir).expect("Error creating espanso data directory");
+    espanso_dir
+}
+
+pub fn get_config_dir() -> PathBuf {
+    // For compatibility purposes, check if the $HOME/.espanso directory is available
+    let home_dir = dirs::home_dir().expect("Can't obtain the user home directory, terminating.");
+    let legacy_espanso_dir = home_dir.join(".espanso");
+    if legacy_espanso_dir.exists() {
+        eprintln!("WARNING: using legacy espanso config location in $HOME/.espanso is DEPRECATED");
+        eprintln!("Starting from espanso v0.3.0, espanso config location is changed.");
+        eprintln!("Please check out the documentation to find out more: https://espanso.org/docs/configuration/");
+
+        return legacy_espanso_dir;
+    }
+
+    // New config location, from version v0.3.0
+    // Refer to issue #73 for more information: https://github.com/federico-terzi/espanso/issues/73
+    let config_dir = dirs::config_dir().expect("Can't obtain config_dir(), terminating.");
+    let espanso_dir = config_dir.join("espanso");
+    create_dir_all(&espanso_dir).expect("Error creating espanso config directory");
+    espanso_dir
+}
+
+const PACKAGES_FOLDER_NAME : &str = "packages";
+
+pub fn get_package_dir() -> PathBuf {
+    // Deprecated $HOME/.espanso/packages directory compatibility check
+    let home_dir = dirs::home_dir().expect("Can't obtain the user home directory, terminating.");
+    let legacy_espanso_dir = home_dir.join(".espanso");
+    if legacy_espanso_dir.exists() {
+        let legacy_package_dir = legacy_espanso_dir.join(PACKAGES_FOLDER_NAME);
+        if legacy_package_dir.exists() {
+            return legacy_package_dir;
+        }
+    }
+
+    // New package location, starting from version v0.3.0
+    let data_dir = get_data_dir();
+    let package_dir = data_dir.join(PACKAGES_FOLDER_NAME);
+    create_dir_all(&package_dir).expect("Error creating espanso packages directory");
+    package_dir
 }
