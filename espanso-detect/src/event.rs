b16d1a04aecf7a26efe5b8fd55f422a6f803575b
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-01-29 21:55:47 +0100

    Fresh start

---
+++ espanso-detect/src/event.rs
@@ -0,0 +1,117 @@
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019-2021 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
+#[derive(Debug)]
+pub enum InputEvent {
+  Mouse(MouseEvent),
+  Keyboard(KeyboardEvent),
+}
+
+#[derive(Debug)]
+pub enum MouseButton {
+  Left,
+  Right,
+  Middle,
+  Button1,
+  Button2,
+  Button3,
+  Button4,
+  Button5,
+}
+
+#[derive(Debug)]
+pub struct MouseEvent {
+  pub button: MouseButton,
+  pub status: Status,
+}
+
+#[derive(Debug)]
+pub enum Status {
+  Pressed,
+  Released,
+}
+
+#[derive(Debug)]
+pub enum Variant {
+  Left,
+  Right,
+}
+
+#[derive(Debug)]
+pub struct KeyboardEvent {
+  pub key: Key,
+  pub value: Option<String>,
+  pub status: Status,
+  pub variant: Option<Variant>,
+}
+
+// A subset of the Web's key values: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+#[derive(Debug)]
+pub enum Key {
+  // Modifiers
+  Alt,
+  CapsLock,
+  Control,
+  Meta,
+  NumLock,
+  Shift,
+
+  // Whitespace
+  Enter,
+  Tab,
+  Space,
+
+  // Navigation
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  End,
+  Home,
+  PageDown,
+  PageUp,
+
+  // Editing keys
+  Backspace,
+
+  // Function keys
+  F1,
+  F2,
+  F3,
+  F4,
+  F5,
+  F6,
+  F7,
+  F8,
+  F9,
+  F10,
+  F11,
+  F12,
+  F13,
+  F14,
+  F15,
+  F16,
+  F17,
+  F18,
+  F19,
+  F20,
+
+  // Other keys, includes the raw code provided by the operating system
+  Other(i32),
+}
---
+++ espanso-detect/src/win32/native.h
@@ -0,0 +1,69 @@
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019-2021 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ESPANSO_DETECT_H
+#define ESPANSO_DETECT_H
+
+#include <stdint.h>
+
+#define INPUT_EVENT_TYPE_KEYBOARD 1
+#define INPUT_EVENT_TYPE_MOUSE    2
+
+#define INPUT_STATUS_PRESSED      1
+#define INPUT_STATUS_RELEASED     2
+
+#define INPUT_LEFT_VARIANT 1
+#define INPUT_RIGHT_VARIANT 2
+
+#define INPUT_MOUSE_LEFT_BUTTON   1
+#define INPUT_MOUSE_RIGHT_BUTTON  2
+#define INPUT_MOUSE_MIDDLE_BUTTON 3
+#define INPUT_MOUSE_BUTTON_1      4
+#define INPUT_MOUSE_BUTTON_2      5
+#define INPUT_MOUSE_BUTTON_3      6
+#define INPUT_MOUSE_BUTTON_4      7
+#define INPUT_MOUSE_BUTTON_5      8
+
+typedef struct {
+  // Keyboard or Mouse event
+  int32_t event_type;
+
+  // Contains the string corresponding to the key, if any
+  uint16_t buffer[24];
+  // Length of the extracted string. Equals 0 if no string is extracted
+  int32_t buffer_len;
+  
+  // Virtual key code of the pressed key in case of keyboard events
+  // Mouse button code otherwise.
+  int32_t key_code;
+  
+  // Left or Right variant
+  int32_t variant;
+
+  // Pressed or Released status
+  int32_t status;
+} InputEvent;
+
+typedef void (*EventCallback)(void * self, InputEvent data);
+extern EventCallback event_callback;
+
+// Initialize the Raw Input API and run the event loop. Blocking call.
+extern "C" int32_t raw_eventloop(void * self, EventCallback callback);
+
+#endif //ESPANSO_DETECT_H
