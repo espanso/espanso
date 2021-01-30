686ceb88da67480313831848088303e3fb6dd7a9
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-01-30 18:41:47 +0100

    First draft of Windows ui-layer

---
+++ espanso-ui/src/win32/native.cpp
@@ -0,0 +1,276 @@
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019-2021 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "native.h"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <array>
+
+#define UNICODE
+
+#ifdef __MINGW32__
+#ifndef WINVER
+#define WINVER 0x0606
+#endif
+#define STRSAFE_NO_DEPRECATE
+#endif
+
+#include <windows.h>
+#include <winuser.h>
+#include <strsafe.h>
+#pragma comment(lib, "Shell32.lib")
+#include <shellapi.h>
+
+#pragma comment(lib, "Gdi32.lib")
+#include <Windows.h>
+
+#define APPWM_ICON_CLICK (WM_APP + 1)
+#define APPWM_SHOW_CONTEXT_MENU (WM_APP + 2)
+#define APPWM_UPDATE_TRAY_ICON (WM_APP + 3)
+
+const wchar_t *const ui_winclass = L"EspansoUI";
+
+typedef struct {
+  UIOptions options;
+  NOTIFYICONDATA nid;
+  HICON g_icons[MAX_ICON_COUNT];
+
+  // Rust interop
+  void *rust_instance;
+  EventCallback event_callback;
+} UIVariables;
+
+
+// Needed to detect when Explorer crashes
+UINT WM_TASKBARCREATED = RegisterWindowMessage(L"TaskbarCreated");
+
+/*
+ * Message handler procedure for the window
+ */
+LRESULT CALLBACK ui_window_procedure(HWND window, unsigned int msg, WPARAM wp, LPARAM lp)
+{
+  UIEvent event = {};
+  UIVariables * variables = reinterpret_cast<UIVariables*>(GetWindowLongPtrW(window, GWLP_USERDATA));
+
+  switch (msg)
+  {
+  case WM_DESTROY:
+    PostQuitMessage(0);
+
+    // Free the tray icons
+    for (int i = 0; i < variables->options.icon_paths_count; i++)
+    {
+      DeleteObject(variables->g_icons[i]);
+    }
+
+    // Free the window variables
+    delete variables;
+    SetWindowLongPtrW(window, GWLP_USERDATA, NULL);
+
+    return 0L;
+  case WM_COMMAND: // Click on the tray icon context menu
+  {
+    UINT idItem = (UINT)LOWORD(wp);
+    UINT flags = (UINT)HIWORD(wp);
+
+    if (flags == 0)
+    {
+      std::cout << "click menu" << std::flush;
+      //context_menu_click_callback(manager_instance, (int32_t)idItem);
+    }
+
+    break;
+  }
+  case APPWM_SHOW_CONTEXT_MENU: // Request to show context menu
+  {
+    HMENU hPopupMenu = CreatePopupMenu();
+
+    // Create the menu
+
+    /*
+            int32_t count = static_cast<int32_t>(lp);
+            std::unique_ptr<MenuItem[]> items(reinterpret_cast<MenuItem*>(wp));
+
+            for (int i = 0; i<count; i++) {
+                if (items[i].type == 1) {
+                    InsertMenu(hPopupMenu, i, MF_BYPOSITION | MF_STRING, items[i].id, items[i].name);
+                }else{
+                    InsertMenu(hPopupMenu, i, MF_BYPOSITION | MF_SEPARATOR, items[i].id, NULL);
+                }
+            }
+
+            POINT pt;
+            GetCursorPos(&pt);
+            SetForegroundWindow(nw);
+            TrackPopupMenu(hPopupMenu, TPM_BOTTOMALIGN | TPM_LEFTALIGN, pt.x, pt.y, 0, nw, NULL);
+            */
+    break;
+  }
+  case APPWM_UPDATE_TRAY_ICON:  // Request to update the tray icon
+  {
+    int32_t index = (int32_t) lp;
+    if (index >= variables->options.icon_paths_count) {
+      break;
+    }
+
+    variables->nid.hIcon = variables->g_icons[index];
+    if (variables->options.show_icon) {
+      Shell_NotifyIcon(NIM_MODIFY, &variables->nid);
+    }
+
+    break;
+  }
+  case APPWM_ICON_CLICK: // Click on the tray icon
+  {
+    switch (lp)
+    {
+    case WM_LBUTTONUP:
+    case WM_RBUTTONUP:
+      event.event_type = UI_EVENT_TYPE_ICON_CLICK;
+      if (variables->event_callback && variables->rust_instance)
+      {
+        variables->event_callback(variables->rust_instance, event);
+      }
+      break;
+    }
+  }
+  default:
+    if (msg == WM_TASKBARCREATED)
+    { // Explorer crashed, recreate the icon
+      if (variables->options.show_icon)
+      {
+        Shell_NotifyIcon(NIM_ADD, &variables->nid);
+      }
+    }
+    return DefWindowProc(window, msg, wp, lp);
+  }
+}
+
+void * ui_initialize(void *_self, UIOptions _options) {
+  HWND window = NULL;
+
+  SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
+
+  // Service Window
+
+  // Docs: https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassexa
+  WNDCLASSEX uiwndclass = {
+      sizeof(WNDCLASSEX),       // cbSize: Size of this structure
+      0,                        // style: Class styles
+      ui_window_procedure,         // lpfnWndProc: Pointer to the window procedure
+      0,                        // cbClsExtra: Number of extra bytes to allocate following the window-class structure
+      0,                        // cbWndExtra: The number of extra bytes to allocate following the window instance.
+      GetModuleHandle(0),       // hInstance: A handle to the instance that contains the window procedure for the class.
+      NULL,                     // hIcon: A handle to the class icon.
+      LoadCursor(0, IDC_ARROW), // hCursor: A handle to the class cursor.
+      NULL,                     // hbrBackground: A handle to the class background brush.
+      NULL,                     // lpszMenuName: Pointer to a null-terminated character string that specifies the resource name of the class menu
+      ui_winclass,              // lpszClassName: A pointer to a null-terminated string or is an atom.
+      NULL                      // hIconSm: A handle to a small icon that is associated with the window class.
+  };
+
+  if (RegisterClassEx(&uiwndclass))
+  {
+    // Initialize the service window
+    window = CreateWindowEx(
+        0,                        // dwExStyle: The extended window style of the window being created.
+        ui_winclass,                 // lpClassName: A null-terminated string or a class atom created by a previous call to the RegisterClass
+        L"Espanso UI Window", // lpWindowName: The window name.
+        WS_OVERLAPPEDWINDOW,      // dwStyle: The style of the window being created.
+        CW_USEDEFAULT,            // X: The initial horizontal position of the window.
+        CW_USEDEFAULT,            // Y: The initial vertical position of the window.
+        100,                      // nWidth: The width, in device units, of the window.
+        100,                      // nHeight: The height, in device units, of the window.
+        NULL,                     // hWndParent:  handle to the parent or owner window of the window being created.
+        NULL,                     // hMenu: A handle to a menu, or specifies a child-window identifier, depending on the window style.
+        GetModuleHandle(0),       // hInstance: A handle to the instance of the module to be associated with the window.
+        NULL                      // lpParam: Pointer to a value to be passed to the window
+    );
+
+    if (window)
+    {
+      UIVariables * variables = new UIVariables();
+      variables->options = _options;
+      variables->rust_instance = _self;
+      SetWindowLongPtrW(window, GWLP_USERDATA, reinterpret_cast<::LONG_PTR>(variables));
+
+      // Load the tray icons
+      for (int i = 0; i < variables->options.icon_paths_count; i++)
+      {
+        variables->g_icons[i] = (HICON)LoadImage(NULL, variables->options.icon_paths[i], IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR | LR_SHARED | LR_DEFAULTSIZE | LR_LOADFROMFILE);
+      }
+
+      // Hide the window
+      ShowWindow(window, SW_HIDE);
+
+      // Setup the icon in the notification space
+      SendMessage(window, WM_SETICON, ICON_BIG, (LPARAM)variables->g_icons[0]);
+      SendMessage(window, WM_SETICON, ICON_SMALL, (LPARAM)variables->g_icons[0]);
+
+      // Notification
+      variables->nid.cbSize = sizeof(variables->nid);
+      variables->nid.hWnd = window;
+      variables->nid.uID = 1;
+      variables->nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE;
+      variables->nid.uCallbackMessage = APPWM_ICON_CLICK;
+      variables->nid.hIcon = variables->g_icons[0];
+      StringCchCopyW(variables->nid.szTip, ARRAYSIZE(variables->nid.szTip), L"espanso");
+
+      // Show the tray icon
+      if (variables->options.show_icon)
+      {
+        Shell_NotifyIcon(NIM_ADD, &variables->nid);
+      }
+    }
+  }
+  
+  return window;
+}
+
+int32_t ui_eventloop(void * window, EventCallback _callback)
+{
+  if (window)
+  {
+    UIVariables * variables = reinterpret_cast<UIVariables*>(GetWindowLongPtrW((HWND) window, GWLP_USERDATA));
+    variables->event_callback = _callback;
+    
+    // Enter the Event loop
+    MSG msg;
+    while (GetMessage(&msg, 0, 0, 0))
+      DispatchMessage(&msg);
+  }
+
+  return 1;
+}
+
+int32_t ui_destroy(void * window) {
+  if (window) {
+    return DestroyWindow((HWND) window);
+  }
+}
+
+void ui_update_tray_icon(void * window, int32_t index)
+{
+  if (window) {
+    PostMessage((HWND) window, APPWM_UPDATE_TRAY_ICON, 0, index);
+  }
+}
---
+++ espanso-ui/src/win32/native.h
@@ -0,0 +1,59 @@
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019-2021 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ESPANSO_UI_H
+#define ESPANSO_UI_H
+
+#include <stdint.h>
+
+// Explicitly define this constant as we need to use it from the Rust side
+// https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation
+#define MAX_FILE_PATH 260
+#define MAX_ICON_COUNT 3
+
+#define UI_EVENT_TYPE_ICON_CLICK 1
+#define UI_EVENT_TYPE_CONTEXT_MENU_CLICK 2
+
+typedef struct {
+  int32_t show_icon;
+
+  wchar_t icon_paths[MAX_ICON_COUNT][MAX_FILE_PATH];
+  int32_t icon_paths_count;
+} UIOptions;
+
+typedef struct {
+  int32_t event_type;
+} UIEvent;
+
+typedef void (*EventCallback)(void * self, UIEvent data);
+
+// Initialize the hidden UI window, the tray icon and returns the window handle.
+extern "C" void * ui_initialize(void * self, UIOptions options);
+
+// Run the event loop. Blocking call.
+extern "C" int32_t ui_eventloop(void * window, EventCallback callback);
+
+// Destroy the given window.
+extern "C" int32_t ui_destroy(void * window);
+
+// Updates the tray icon to the given one. The method accepts an index that refers to
+// the icon within the UIOptions.icon_paths array.
+extern "C" void ui_update_tray_icon(void * window, int32_t index);
+
+#endif //ESPANSO_UI_H
