02f53706d44cfe498c8a6c18e0be83e410a4ca85
Federico Terzi <federicoterzi96@gmail.com>
2021-03-08 16:36:16 +0100

    Add config store base implementation and tests

--- espanso-config/src/config/mod.rs
+++ espanso-config/src/config/mod.rs
@@ -4,8 +4,22 @@ mod path;
 mod parse;
 mod util;
 mod resolve;
+mod store;
 
 pub trait Config {
   fn label(&self) -> &str;
   fn match_paths(&self) -> &HashSet<String>;
+
+  fn is_match(&self, app: &AppProperties) -> bool;
+}
+
+pub trait ConfigStore<'a> {
+  fn default(&'a self) -> &'a dyn Config;
+  fn active(&'a self, app: &AppProperties) -> &'a dyn Config;
+}
+
+pub struct AppProperties<'a> {
+  pub title: Option<&'a str>,
+  pub class: Option<&'a str>,
+  pub exec: Option<&'a str>,
 }
