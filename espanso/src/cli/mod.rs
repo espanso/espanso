1f0fe74ac10c2cb6c0203afa9efad3dd24eec257
Federico Terzi <federicoterzi96@gmail.com>
2021-03-28 18:22:50 +0200

    feat(core): implement basic cli handler structure and path handler

---
+++ espanso/src/cli/mod.rs
@@ -0,0 +1,65 @@
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019-2021 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use clap::ArgMatches;
+use espanso_config::{config::ConfigStore, matches::store::MatchStore};
+use espanso_path::Paths;
+
+pub mod log;
+pub mod path;
+
+pub struct CliModule {
+  pub enable_logs: bool,
+  pub requires_paths: bool,
+  pub requires_config: bool,
+  pub subcommand: String,
+  pub entry: fn(CliModuleArgs),
+}
+
+impl Default for CliModule {
+  fn default() -> Self {
+    Self {
+      enable_logs: false,
+      requires_paths: false, 
+      requires_config: false, 
+      subcommand: "".to_string(), 
+      entry: |_| {},
+    }
+  }
+}
+
+pub struct CliModuleArgs {
+  pub config_store: Option<Box<dyn ConfigStore>>,
+  pub match_store: Option<Box<dyn MatchStore>>,
+  pub is_legacy_config: bool,
+  pub paths: Option<Paths>,
+  pub cli_args: Option<ArgMatches<'static>>,
+}
+
+impl Default for CliModuleArgs {
+  fn default() -> Self {
+    Self {
+      config_store: None,
+      match_store: None,
+      is_legacy_config: false,
+      paths: None,
+      cli_args: None,
+    }
+  }
+}
