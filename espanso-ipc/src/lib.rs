eb980f19d0013b648f20dc7d33afaf5c2875fd80
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-06-02 21:12:56 +0200

    feat(ipc): refactor IPC on Windows

--- espanso-ipc/src/lib.rs
+++ espanso-ipc/src/lib.rs
@@ -18,9 +18,8 @@
  */
 
 use anyhow::Result;
-use crossbeam::channel::{unbounded, Receiver};
 use serde::{de::DeserializeOwned, Serialize};
-use std::path::Path;
+use std::{path::Path};
 use thiserror::Error;
 
 #[cfg(target_os = "windows")]
@@ -29,13 +28,22 @@ pub mod windows;
 #[cfg(not(target_os = "windows"))]
 pub mod unix;
 
+pub type EventHandler<Event> = Box<dyn Fn(Event) -> EventHandlerResponse<Event>>;
+
+pub enum EventHandlerResponse<Event> {
+  NoResponse,
+  Response(Event),
+  Error(anyhow::Error),
+  Exit,
+}
+
 pub trait IPCServer<Event> {
-  fn run(&self) -> Result<()>;
-  fn accept_one(&self) -> Result<()>;
+  fn run(self, handler: EventHandler<Event>) -> Result<()>;
 }
 
 pub trait IPCClient<Event> {
-  fn send(&self, event: Event) -> Result<()>;
+  fn send_sync(&mut self, event: Event) -> Result<Event>;
+  fn send_async(&mut self, event: Event) -> Result<()>;
 }
 
 #[cfg(not(target_os = "windows"))]
@@ -55,17 +63,16 @@ pub fn client<Event: Serialize>(id: &str, parent_dir: &Path) -> Result<impl IPCC
 }
 
 #[cfg(target_os = "windows")]
-pub fn server<Event: Send + Sync + DeserializeOwned>(
+pub fn server<Event: Send + Sync + DeserializeOwned + Serialize>(
   id: &str,
   _: &Path,
-) -> Result<(impl IPCServer<Event>, Receiver<Event>)> {
-  let (sender, receiver) = unbounded();
-  let server = windows::WinIPCServer::new(id, sender)?;
-  Ok((server, receiver))
+) -> Result<impl IPCServer<Event>> {
+  let server = windows::WinIPCServer::new(id)?;
+  Ok(server)
 }
 
 #[cfg(target_os = "windows")]
-pub fn client<Event: Serialize>(id: &str, _: &Path) -> Result<impl IPCClient<Event>> {
+pub fn client<Event: Serialize + DeserializeOwned>(id: &str, _: &Path) -> Result<impl IPCClient<Event>> {
   let client = windows::WinIPCClient::new(id)?;
   Ok(client)
 }
@@ -73,45 +80,184 @@ pub fn client<Event: Serialize>(id: &str, _: &Path) -> Result<impl IPCClient<Eve
 #[derive(Error, Debug)]
 pub enum IPCServerError {
   #[error("stream ended")]
-  StreamEnded(#[from] std::io::Error),
+  StreamEnded,
 
-  #[error("send failed")]
-  SendFailed(),
+  #[error("handler reported error `{0}`")]
+  HandlerError(#[from] anyhow::Error),
+}
+
+#[derive(Error, Debug)]
+pub enum IPCClientError {
+  #[error("empty response")]
+  EmptyResponse,
+
+  #[error("malformed response received `{0}`")]
+  MalformedResponse(#[from] anyhow::Error),
+
+  #[error("message response timed out")]
+  Timeout,
 }
 
 #[cfg(test)]
 mod tests {
+  use std::{
+    sync::{mpsc::channel},
+  };
+
   use super::*;
   use serde::{Deserialize, Serialize};
 
   #[derive(Serialize, Deserialize)]
   enum Event {
-    Bar,
-    Foo(String),
+    Async,
+    Sync(String),
+    SyncResult(String),
+    ExitRequest,
   }
 
   #[test]
-  fn ipc_works_correctly() {
-    let (server, receiver) = server::<Event>("testespansoipc", &std::env::temp_dir()).unwrap();
-    let server_handle = std::thread::spawn(move || {
-      server.accept_one().unwrap();
+  fn ipc_async_message() {
+    let server = server::<Event>("testespansoipcasync", &std::env::temp_dir()).unwrap();
+    
+    let client_handle = std::thread::spawn(move || {
+      let mut client = client::<Event>("testespansoipcasync", &std::env::temp_dir()).unwrap();
+
+      client.send_async(Event::Async).unwrap();
+      client.send_async(Event::ExitRequest).unwrap();
     });
 
-    // TODO: avoid delay and change the IPC code so that we can wait for the IPC
-    std::thread::sleep(std::time::Duration::from_millis(300));
+    server
+      .run(Box::new(move |event| match event {
+        Event::ExitRequest => EventHandlerResponse::Exit,
+        evt => {
+          assert!(matches!(evt, Event::Async));
+          EventHandlerResponse::NoResponse
+        }
+      }))
+      .unwrap();
 
-    let client = client::<Event>("testespansoipc", &std::env::temp_dir()).unwrap();
-    client.send(Event::Foo("hello".to_string())).unwrap();
-
-    let event = receiver.recv().unwrap();
-    assert!(matches!(event, Event::Foo(x) if x == "hello"));
-
-    server_handle.join().unwrap();
+    client_handle.join().unwrap();
   }
 
   #[test]
-  fn ipc_client_fails_to_send() {
-    let client = client::<Event>("testespansoipc", &std::env::temp_dir()).unwrap();
-    assert!(client.send(Event::Foo("hello".to_string())).is_err());
+  fn ipc_sync_message() {
+    let server = server::<Event>("testespansoipcsync", &std::env::temp_dir()).unwrap();
+    
+    let client_handle = std::thread::spawn(move || {
+      let mut client = client::<Event>("testespansoipcsync", &std::env::temp_dir()).unwrap();
+
+      let response = client.send_sync(Event::Sync("test".to_owned())).unwrap();
+      client.send_async(Event::ExitRequest).unwrap();
+
+      assert!(matches!(response, Event::SyncResult(s) if s == "test"));
+    });
+
+    server
+      .run(Box::new(move |event| match event {
+        Event::ExitRequest => EventHandlerResponse::Exit,
+        Event::Sync(s) => EventHandlerResponse::Response(Event::SyncResult(s)),
+        _ => EventHandlerResponse::NoResponse,
+      }))
+      .unwrap();
+
+    client_handle.join().unwrap();
+  }
+
+  #[test]
+  fn ipc_multiple_sync_with_delay_message() {
+    let server = server::<Event>("testespansoipcmultiplesync", &std::env::temp_dir()).unwrap();
+    
+    let client_handle = std::thread::spawn(move || {
+      let mut client = client::<Event>("testespansoipcmultiplesync", &std::env::temp_dir()).unwrap();
+
+      let response = client.send_sync(Event::Sync("test".to_owned())).unwrap();
+
+      std::thread::sleep(std::time::Duration::from_millis(500));
+
+      let response2 = client.send_sync(Event::Sync("test2".to_owned())).unwrap();
+      client.send_async(Event::ExitRequest).unwrap();
+
+      assert!(matches!(response, Event::SyncResult(s) if s == "test"));
+      assert!(matches!(response2, Event::SyncResult(s) if s == "test2"));
+    });
+
+    server
+      .run(Box::new(move |event| match event {
+        Event::ExitRequest => EventHandlerResponse::Exit,
+        Event::Sync(s) => EventHandlerResponse::Response(Event::SyncResult(s)),
+        _ => EventHandlerResponse::NoResponse,
+      }))
+      .unwrap();
+
+    client_handle.join().unwrap();
+  }
+
+  #[test]
+  fn ipc_multiple_clients() {
+    let server = server::<Event>("testespansoipcmultiple", &std::env::temp_dir()).unwrap();
+
+    let (tx, rx) = channel();
+    
+    let client_handle = std::thread::spawn(move || {
+      let mut client = client::<Event>("testespansoipcmultiple", &std::env::temp_dir()).unwrap();
+
+      let response = client.send_sync(Event::Sync("client1".to_owned())).unwrap();
+
+      tx.send(()).unwrap();
+
+      assert!(matches!(response, Event::SyncResult(s) if s == "client1"));
+    });
+
+    let client_handle2 = std::thread::spawn(move || {
+      let mut client = client::<Event>("testespansoipcmultiple", &std::env::temp_dir()).unwrap();
+
+      let response = client.send_sync(Event::Sync("client2".to_owned())).unwrap();
+
+      // Wait for the other client before terminating
+      rx.recv().unwrap();
+
+      client.send_async(Event::ExitRequest).unwrap();
+
+      assert!(matches!(response, Event::SyncResult(s) if s == "client2"));
+    });
+
+    server
+      .run(Box::new(move |event| match event {
+        Event::ExitRequest => EventHandlerResponse::Exit,
+        Event::Sync(s) => EventHandlerResponse::Response(Event::SyncResult(s)),
+        _ => EventHandlerResponse::NoResponse,
+      }))
+      .unwrap();
+
+    client_handle.join().unwrap();
+    client_handle2.join().unwrap();
+  }
+
+   #[test]
+  fn ipc_sync_big_payload_message() {
+    let server = server::<Event>("testespansoipcsyncbig", &std::env::temp_dir()).unwrap();
+    
+    let client_handle = std::thread::spawn(move || {
+      let mut client = client::<Event>("testespansoipcsyncbig", &std::env::temp_dir()).unwrap();
+
+      let mut payload = String::new();
+      for _ in 0..10000 {
+        payload.push_str("log string repeated");
+      }
+      let response = client.send_sync(Event::Sync(payload.clone())).unwrap();
+      client.send_async(Event::ExitRequest).unwrap();
+
+      assert!(matches!(response, Event::SyncResult(s) if s == payload));
+    });
+
+    server
+      .run(Box::new(move |event| match event {
+        Event::ExitRequest => EventHandlerResponse::Exit,
+        Event::Sync(s) => EventHandlerResponse::Response(Event::SyncResult(s)),
+        _ => EventHandlerResponse::NoResponse,
+      }))
+      .unwrap();
+
+    client_handle.join().unwrap();
   }
 }
