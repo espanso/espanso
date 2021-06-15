da22cecff541b1114924b39d68f36e89e8615b1f
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-06-15 23:29:04 +0200

    feat(core): implement wizard migration handler

--- espanso/src/cli/launcher/util.rs
+++ espanso/src/cli/launcher/util.rs
@@ -17,15 +17,62 @@
  * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use std::path::Path;
+use std::{path::Path, process::Command};
 
-use crate::lock::acquire_legacy_lock;
+use anyhow::Result;
+use thiserror::Error;
+use espanso_path::Paths;
+
+use crate::{exit_code::{MIGRATE_CLEAN_FAILURE, MIGRATE_DIRTY_FAILURE}, lock::acquire_legacy_lock};
 
 pub fn is_legacy_version_running(runtime_path: &Path) -> bool {
   let legacy_lock_file = acquire_legacy_lock(runtime_path);
   if legacy_lock_file.is_none() {
-    true 
+    true
   } else {
-    false 
+    false
   }
+}
+
+pub fn migrate_configuration(paths: &Paths) -> Result<()> {
+  let espanso_exe_path = std::env::current_exe()?;
+  let mut command = Command::new(&espanso_exe_path.to_string_lossy().to_string());
+  command.args(&["migrate", "--noconfirm"]);
+  command.env(
+    "ESPANSO_CONFIG_DIR",
+    paths.config.to_string_lossy().to_string(),
+  );
+  command.env(
+    "ESPANSO_PACKAGE_DIR",
+    paths.packages.to_string_lossy().to_string(),
+  );
+  command.env(
+    "ESPANSO_RUNTIME_DIR",
+    paths.runtime.to_string_lossy().to_string(),
+  );
+
+  let mut child = command.spawn()?;
+  let result = child.wait()?;
+
+  if result.success() {
+    Ok(())
+  } else {
+    match result.code() {
+      Some(code) if code == MIGRATE_CLEAN_FAILURE => Err(MigrationError::CleanError.into()),
+      Some(code) if code == MIGRATE_DIRTY_FAILURE=> Err(MigrationError::DirtyError.into()),
+      _ => Err(MigrationError::UnexpectedError.into())
+    }
+  }
+}
+
+#[derive(Error, Debug)]
+pub enum MigrationError {
+  #[error("clean error")]
+  CleanError,
+
+  #[error("dirty error")]
+  DirtyError,
+
+  #[error("unexpected error")]
+  UnexpectedError,
 }
