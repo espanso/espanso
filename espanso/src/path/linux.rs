47f294674f7dbd84c98416b04ac9d84a57a96f9d
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-06-27 15:46:09 +0200

    feat(core): extend env-path module to Windows

--- espanso/src/path/linux.rs
+++ espanso/src/path/linux.rs
@@ -17,29 +17,19 @@
  * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#[cfg(target_os = "macos")]
-mod macos;
-#[cfg(target_os = "macos")]
-pub use macos::add_espanso_to_path;
-#[cfg(target_os = "macos")]
-pub use macos::remove_espanso_from_path;
-#[cfg(target_os = "macos")]
-pub use macos::is_espanso_in_path;
+use anyhow::Result;
 
-#[cfg(target_os = "windows")]
-mod win;
-#[cfg(target_os = "windows")]
-pub use win::add_espanso_to_path;
-#[cfg(target_os = "windows")]
-pub use win::remove_espanso_from_path;
-#[cfg(target_os = "windows")]
-pub use win::is_espanso_in_path;
+pub fn is_espanso_in_path() -> bool {
+  // Not supported on Linux
+  true
+}
 
-#[cfg(target_os = "linux")]
-mod linux;
-#[cfg(target_os = "linux")]
-pub use linux::add_espanso_to_path;
-#[cfg(target_os = "linux")]
-pub use linux::remove_espanso_from_path;
-#[cfg(target_os = "linux")]
-pub use linux::is_espanso_in_path;
+pub fn add_espanso_to_path(_: bool) -> Result<()> {
+  // Not supported on Linux
+  Ok(())
+}
+
+pub fn remove_espanso_from_path(_: bool) -> Result<()> {
+  // Not supported on Linux
+  Ok(())
+}
