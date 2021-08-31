dfa6a40b6c7bc3c77620ff1287c1643efaa2b98d
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-08-31 22:57:22 +0200

    feat(package): progress in Github and Gitlab package providers

--- espanso-package/src/lib.rs
+++ espanso-package/src/lib.rs
@@ -19,19 +19,20 @@
 
 use std::path::Path;
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 use thiserror::Error;
 
 mod manifest;
 mod package;
 mod provider;
 mod resolver;
+mod util;
 
 #[derive(Debug, Default)]
 pub struct PackageSpecifier {
   pub name: String,
   pub version: Option<String>,
-  
+
   // Source information
   pub git_repo_url: Option<String>,
   pub git_branch: Option<String>,
@@ -51,8 +52,7 @@ pub trait Package {
 
 pub trait PackageProvider {
   fn download(&self, package: &PackageSpecifier) -> Result<Box<dyn Package>>;
-  // TODO: fn check update available?
-  // TODO: fn update
+  // TODO: fn check update available? (probably should be only available in the hub)
 }
 
 // TODO: once the download is completed, avoid copying files beginning with "."
@@ -61,4 +61,45 @@ pub trait PackageProvider {
 pub enum PackageResolutionError {
   #[error("package not found")]
   PackageNotFound,
+}
+
+pub fn get_provider(package: &PackageSpecifier) -> Result<Box<dyn PackageProvider>> {
+  if let Some(git_repo_url) = package.git_repo_url.as_deref() {
+    let matches_known_hosts = if let Some(github_parts) = util::github::extract_github_url_parts(git_repo_url) {
+      if let Some(repo_scheme) =
+        util::github::resolve_repo_scheme(github_parts, package.git_branch.as_deref())?
+      {
+        return Ok(Box::new(provider::github::GitHubPackageProvider::new(
+          repo_scheme.author,
+          repo_scheme.name,
+          repo_scheme.branch,
+        )));
+      }
+
+      true
+    } else if let Some(gitlab_parts) = util::gitlab::extract_gitlab_url_parts(git_repo_url) {
+      panic!("GitLab is not supported yet!");
+      todo!();
+
+      true
+    } else {
+      false
+    };
+
+    // Git repository seems to be in one of the known hosts, but the direct methods
+    // couldn't retrieve its content. This might happen with private repos (as they are not
+    // available to non-authenticated requests), so we check if a "git ls-remote" command 
+    // is able to access it.
+    if matches_known_hosts && !util::git::is_private_repo(git_repo_url) {
+      bail!("could not access repository: {}, make sure it exists and that you have the necessary access rights.");
+    }
+
+    // Git repository is neither on Github or Gitlab
+    // OR it's a private repository, which means we can't use the direct method
+    // (because it's not authenticated)
+    Ok(Box::new(provider::git::GitPackageProvider::new()))
+  } else {
+    // TODO: use espanso-hub method
+    todo!();
+  }
 }
---
+++ espanso-package/src/util/github.rs
@@ -0,0 +1,118 @@
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019-2021 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use anyhow::Result;
+use lazy_static::lazy_static;
+use regex::Regex;
+use reqwest::StatusCode;
+
+lazy_static! {
+  static ref GITHUB_REGEX: Regex = Regex::new(r"(https://github.com/|git@github.com:)(?P<author>.*?)/(?P<name>.*?)(\.|$)").unwrap();
+}
+
+#[derive(Debug, PartialEq)]
+pub struct GitHubParts {
+  author: String,
+  name: String,
+}
+
+pub fn extract_github_url_parts(url: &str) -> Option<GitHubParts> {
+  let captures = GITHUB_REGEX.captures(url)?;
+  let author = captures.name("author")?;
+  let name = captures.name("name")?;
+  
+  Some(GitHubParts {
+    author: author.as_str().to_string(),
+    name: name.as_str().to_string(),
+  })
+}
+
+pub struct ResolvedRepoScheme {
+  pub author: String,
+  pub name: String,
+  pub branch: String,
+}
+
+pub fn resolve_repo_scheme(parts: GitHubParts, force_branch: Option<&str>) -> Result<Option<ResolvedRepoScheme>> {
+  if let Some(force_branch) = force_branch {
+    if check_repo_with_branch(&parts, force_branch)? {
+      return Ok(Some(ResolvedRepoScheme {
+        author: parts.author,
+        name: parts.name,
+        branch: force_branch.to_string(),
+      }))
+    }
+  } else {
+    if check_repo_with_branch(&parts, "master")? {
+      return Ok(Some(ResolvedRepoScheme {
+        author: parts.author,
+        name: parts.name,
+        branch: "master".to_string(),
+      }));
+    }
+
+    if check_repo_with_branch(&parts, "main")? {
+      return Ok(Some(ResolvedRepoScheme {
+        author: parts.author,
+        name: parts.name,
+        branch: "main".to_string(),
+      }));
+    }
+  }
+
+  Ok(None)
+}
+
+pub fn check_repo_with_branch(parts: &GitHubParts, branch: &str) -> Result<bool> {
+  let client = reqwest::blocking::Client::new();
+
+  let url = generate_github_download_url(parts, branch);
+  let response = client.head(url).send()?;
+
+  Ok(response.status() == StatusCode::FOUND)
+}
+
+fn generate_github_download_url(parts: &GitHubParts, branch: &str) -> String {
+  format!("https://github.com/{}/{}/archive/refs/heads/{}.zip", parts.author, parts.name, branch)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_extract_github_url_parts() {
+    assert_eq!(extract_github_url_parts("https://github.com/federico-terzi/espanso").unwrap(), GitHubParts {
+      author: "federico-terzi".to_string(),
+      name: "espanso".to_string(),
+    });
+
+    assert_eq!(extract_github_url_parts("https://github.com/federico-terzi/espanso.git").unwrap(), GitHubParts {
+      author: "federico-terzi".to_string(),
+      name: "espanso".to_string(),
+    });
+
+    assert_eq!(extract_github_url_parts("git@github.com:federico-terzi/espanso.git").unwrap(), GitHubParts {
+      author: "federico-terzi".to_string(),
+      name: "espanso".to_string(),
+    });
+
+    assert_eq!(extract_github_url_parts("https://gitlab.com/federicoterzi/espanso-test-package/").is_none(), true);
+  }
+}
