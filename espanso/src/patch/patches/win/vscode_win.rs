5916cbdf6ef3c479b2a2639789e99ed5f0fb35f6
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-09-25 15:56:28 +0200

    feat(core): add VSCode patch on Windows

--- espanso/src/patch/patches/win/vscode_win.rs
+++ espanso/src/patch/patches/win/vscode_win.rs
@@ -17,5 +17,28 @@
  * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-pub mod onenote_for_windows_10;
-pub mod vscode_win;
+use std::sync::Arc;
+
+use espanso_config::config::Backend;
+
+use crate::patch::patches::{PatchedConfig, Patches};
+use crate::patch::PatchDefinition;
+
+pub fn patch() -> PatchDefinition {
+  PatchDefinition {
+    name: module_path!().split(":").last().unwrap_or("unknown"),
+    is_enabled: || cfg!(target_os = "windows"),
+    should_patch: |app| app.exec.unwrap_or_default().contains("Code.exe"),
+    apply: |base, name| {
+      Arc::new(PatchedConfig::patch(
+        base,
+        name,
+        Patches {
+          key_delay: Some(Some(10)),
+          backend: Some(Backend::Clipboard),
+          ..Default::default()
+        },
+      ))
+    },
+  }
+}
