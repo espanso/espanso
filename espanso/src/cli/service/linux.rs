d75c5f038bac17a95b3a121e5be980e7f6d5b4c9
Federico Terzi <federicoterzi96@gmail.com>
2021-10-06 19:17:44 +0200

    fix(core): fix warnings

--- espanso/src/cli/service/linux.rs
+++ espanso/src/cli/service/linux.rs
@@ -29,7 +29,7 @@ use crate::{error_eprintln, info_println, warn_eprintln};
 
 const LINUX_SERVICE_NAME: &str = "espanso";
 const LINUX_SERVICE_CONTENT: &str = include_str!("../../res/linux/systemd.service");
-const LINUX_SERVICE_FILENAME: &str = &formatcp!("{}.service", LINUX_SERVICE_NAME);
+const LINUX_SERVICE_FILENAME: &str = formatcp!("{}.service", LINUX_SERVICE_NAME);
 
 pub fn register() -> Result<()> {
   let service_file = get_service_file_path()?;
