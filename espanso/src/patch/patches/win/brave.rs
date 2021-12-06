2a036380137757956bf8d6f82de9bb3932025d79
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-12-06 22:50:15 +0100

    fix(core): add patch for brave browser. Fix #876

--- espanso/src/patch/patches/win/brave.rs
+++ espanso/src/patch/patches/win/brave.rs
@@ -17,6 +17,25 @@
  * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-pub mod brave;
-pub mod onenote_for_windows_10;
-pub mod vscode_win;
+use std::sync::Arc;
+
+use crate::patch::patches::{PatchedConfig, Patches};
+use crate::patch::PatchDefinition;
+
+pub fn patch() -> PatchDefinition {
+  PatchDefinition {
+    name: module_path!().split(':').last().unwrap_or("unknown"),
+    is_enabled: || cfg!(target_os = "windows"),
+    should_patch: |app| app.exec.unwrap_or_default().contains("brave.exe"),
+    apply: |base, name| {
+      Arc::new(PatchedConfig::patch(
+        base,
+        name,
+        Patches {
+          pre_paste_delay: Some(400),
+          ..Default::default()
+        },
+      ))
+    },
+  }
+}
