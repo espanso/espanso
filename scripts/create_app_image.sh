ea35c8aecea2be464b63879b5f0b34440212e5a7
Federico Terzi <federicoterzi96@gmail.com>
2021-12-21 20:46:25 +0100

    fix(misc): apply workaround to hopefully solve #900

--- scripts/create_app_image.sh
+++ scripts/create_app_image.sh
@@ -8,12 +8,20 @@ BASE_DIR=$(pwd)
 
 mkdir -p $TOOL_DIR
 
-if ls $TOOL_DIR/*.AppImage 1> /dev/null 2>&1; then
+if ls $TOOL_DIR/linuxdeploy*.AppImage 1> /dev/null 2>&1; then
   echo "Skipping download of linuxdeploy"
 else
   echo "Downloading linuxdeploy tool"
   wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -P "$TOOL_DIR"
   chmod +x $TOOL_DIR/linuxdeploy*.AppImage
+fi
+
+if ls $TOOL_DIR/appimagetool*.AppImage 1> /dev/null 2>&1; then
+  echo "Skipping download of appimagetool"
+else
+  echo "Downloading appimagetool"
+  wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -P "$TOOL_DIR"
+  chmod +x $TOOL_DIR/appimagetool*.AppImage
 fi
 
 rm -Rf "$TARGET_DIR"
@@ -29,4 +37,16 @@ $TOOL_DIR/linuxdeploy*.AppImage --appimage-extract-and-run -e "$BASE_DIR/$EXEC_P
   --appdir $BUILD_DIR \
   --output appimage
 chmod +x ./Espanso*.AppImage
+
+# Apply a workaround to fix this issue: https://github.com/federico-terzi/espanso/issues/900
+# See: https://github.com/project-slippi/Ishiiruka/issues/323#issuecomment-977415376
+
+echo "Applying patch for libgmodule"
+
+./Espanso*.AppImage --appimage-extract
+rm -Rf ./Espanso*.AppImage
+rm -Rf squashfs-root/usr/lib/libgmodule*
+$TOOL_DIR/appimagetool*.AppImage -v squashfs-root
+rm -Rf squashfs-root
+
 popd
