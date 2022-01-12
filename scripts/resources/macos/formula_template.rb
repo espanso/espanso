50a67baf03a39bf777259cce5934affcdf2a867d
Federico Terzi <federicoterzi96@gmail.com>
2022-01-12 21:27:40 +0100

    feat(misc): add scripts to publish homebrew cask

---
+++ scripts/resources/macos/formula_template.rb
@@ -0,0 +1,19 @@
+cask "espanso" do
+  version "{{{VERSION}}}"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/federico-terzi/espanso/releases/download/v#{version}/Espanso-Mac-Intel.zip"
+    sha256 "{{{INTEL_SHA}}}"
+  else
+    url "https://github.com/federico-terzi/espanso/releases/download/v#{version}/Espanso-Mac-M1.zip"
+    sha256 "{{{M1_SHA}}}"
+  end
+
+  name "Espanso"
+  desc "A Privacy-first, Cross-platform Text Expander"
+  homepage "https://espanso.org/"
+
+  app "Espanso.app"
+
+  zap trash: "~/Library/Caches/espanso"
+end
