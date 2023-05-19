d30d14cd81e13514b072d350613c27e63deb4a10
leke-ariyo <lekeariyo2015@gmail.com>
2023-05-19 12:50:02 +0300

    chore: optimized bash scripts

--- scripts/create_bundle.sh
+++ scripts/create_bundle.sh
@@ -1,24 +1,14 @@
 set -e
 
-FINAL_EXEC_PATH=$EXEC_PATH
-
-if [ $BUILD_ARCH != "current" ]; then
-  FINAL_EXEC_PATH=$(echo $EXEC_PATH | sed "s/target\//target\/$BUILD_ARCH\//g")
-fi
+[ $BUILD_ARCH != "current" ] && FINAL_EXEC_PATH=$(echo $EXEC_PATH | sed "s/target\//target\/$BUILD_ARCH\//g") || FINAL_EXEC_PATH=$EXEC_PATH
 
 TARGET_DIR=target/mac/Espanso.app
+VERSION=$(awk -F '"' '/version/ { print $2; exit; }' espanso/Cargo.toml)
 
 rm -Rf $TARGET_DIR
+mkdir -p $TARGET_DIR/Contents/MacOS $TARGET_DIR/Contents/Resources
 
-VERSION=$(cat espanso/Cargo.toml | grep version | head -1 | awk -F '"' '{ print $2 }')
-
-mkdir -p $TARGET_DIR/Contents
-mkdir -p $TARGET_DIR/Contents/MacOS
-mkdir -p $TARGET_DIR/Contents/Resources
-
-sed	-e "s/VERSION/$VERSION/" espanso/src/res/macos/Info.plist > $TARGET_DIR/Contents/Info.plist
-
-/bin/echo "APPL????" > $TARGET_DIR/Contents/PkgInfo
-
-cp -f espanso/src/res/macos/icon.icns $TARGET_DIR/Contents/Resources/icon.icns
+sed -e "s/VERSION/$VERSION/" espanso/src/res/macos/Info.plist > $TARGET_DIR/Contents/Info.plist
+echo "APPL????" > $TARGET_DIR/Contents/PkgInfo
+cp -f espanso/src/res/macos/icon.icns $TARGET_DIR/Contents/Resources/
 cp -f $FINAL_EXEC_PATH $TARGET_DIR/Contents/MacOS/espanso
--- scripts/publish_homebrew_version.sh
+++ scripts/publish_homebrew_version.sh
@@ -6,7 +6,6 @@ if [[ -z "$VERSION" ]]; then
   echo "Missing target VERSION environment variable, please specify it"
   exit 1
 fi
-
 # Removing the v suffix, if present
 VERSION=${VERSION#"v"}
 
@@ -19,19 +18,18 @@ echo "Downloading macOS artifacts"
 gh release download v$VERSION --pattern "Espanso-Mac*" --dir target/homebrew/artifacts
 
 echo "Reading artifacts hashes"
-INTEL_SHA=$(cat target/homebrew/artifacts/Espanso-Mac-Intel.zip.sha256.txt | awk -F ' ' '{print $1}')
-M1_SHA=$(cat target/homebrew/artifacts/Espanso-Mac-M1.zip.sha256.txt | awk -F ' ' '{print $1}')
+INTEL_SHA=$(awk '{print $1}' target/homebrew/artifacts/Espanso-Mac-Intel.zip.sha256.txt)
+M1_SHA=$(awk '{print $1}' target/homebrew/artifacts/Espanso-Mac-M1.zip.sha256.txt)
 
 echo "Cloning tap repository"
-
 pushd target/homebrew
 git clone git@github.com:espanso/homebrew-espanso.git
 
 pushd homebrew-espanso
 echo "Rendering formula template"
 
-cat ../../../scripts/resources/macos/formula_template.rb | sed "s/{{{VERSION}}}/$VERSION/g" | \
-  sed "s/{{{INTEL_SHA}}}/$INTEL_SHA/g" | sed "s/{{{M1_SHA}}}/$M1_SHA/g" > ./Casks/espanso.rb
+sed "s/{{{VERSION}}}/$VERSION/g; s/{{{INTEL_SHA}}}/$INTEL_SHA/g; s/{{{M1_SHA}}}/$M1_SHA/g" \
+    ../../../scripts/resources/macos/formula_template.rb > ./Casks/espanso.rb
 
 echo "Committing version update"
 git add Casks/espanso.rb
