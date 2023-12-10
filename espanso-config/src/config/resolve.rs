e4f123a1548396765b8e3162e4f3e6941eddc295
Federico Terzi <federico-terzi@users.noreply.github.com>
2023-12-10 15:26:11 +0100

    fix(config): fix warning

--- espanso-config/src/config/resolve.rs
+++ espanso-config/src/config/resolve.rs
@@ -300,13 +300,10 @@ impl Config for ResolvedConfig {
   }
 
   fn emulate_alt_codes(&self) -> bool {
-    self.parsed.emulate_alt_codes.unwrap_or({
-      if cfg!(target_os = "windows") {
-        true
-      } else {
-        false
-      }
-    })
+    self
+      .parsed
+      .emulate_alt_codes
+      .unwrap_or(cfg!(target_os = "windows"))
   }
 
   fn post_form_delay(&self) -> usize {
