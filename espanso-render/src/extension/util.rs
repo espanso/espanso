b9194b3560f1031532e030fdd01cbc14b8765175
Auca Maillot <aucacoyan@gmail.com>
2023-12-11 19:45:07 -0300

    :rotating_light: Separate numbers with `_` inbetween

--- espanso-render/src/extension/util.rs
+++ espanso-render/src/extension/util.rs
@@ -46,7 +46,7 @@ pub fn set_command_flags(command: &mut Command) {
   use std::os::windows::process::CommandExt;
   // Avoid showing the shell window
   // See: https://github.com/federico-terzi/espanso/issues/249
-  command.creation_flags(0x08000000);
+  command.creation_flags(0x0800_0000);
 }
 
 #[cfg(not(target_os = "windows"))]
