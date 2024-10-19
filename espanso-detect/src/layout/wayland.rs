81eb6414cea6d6213993e3d4b1e3e4c8b237652c
Auca Maillot <aucacoyan@gmail.com>
2024-10-19 12:12:04 -0300

    change a & and a fmt line

--- espanso-detect/src/layout/wayland.rs
+++ espanso-detect/src/layout/wayland.rs
@@ -5,8 +5,8 @@ use std::os::fd::BorrowedFd;
 use wayland_client::Connection;
 use wayland_client::Proxy;
 
-fn pid_from_wayland_display_fd(fd: &BorrowedFd) -> i32 {
-  let soc_peer_process = socket::getsockopt(fd, socket::sockopt::PeerCredentials).unwrap();
+fn pid_from_wayland_display_fd(fd: BorrowedFd) -> i32 {
+  let soc_peer_process = socket::getsockopt(&fd, socket::sockopt::PeerCredentials).unwrap();
   soc_peer_process.pid()
 }
 
@@ -15,11 +15,11 @@ fn get_compositor_pid() -> i32 {
   let wl_display = wl_conn.display();
   let wl_display_backend = wl_display.backend().upgrade().unwrap();
 
-  pid_from_wayland_display_fd(&wl_display_backend.poll_fd())
+  pid_from_wayland_display_fd(wl_display_backend.poll_fd())
 }
 
 fn process_name_from_pid(pid: i32) -> String {
-  let path = format!("/proc/{}/comm", pid);
+  let path = format!("/proc/{pid}/comm");
   let mut file = File::open(path).unwrap();
   let mut buf = String::new();
   file.read_to_string(&mut buf).unwrap();
