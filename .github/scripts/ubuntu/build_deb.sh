446494f74ba4a355fda47ec751f2a88ca68582f9
Auca Coyan <aucacoyan@gmail.com>
2024-12-15 20:02:01 -0300

    :bug: fix missing readme in cargo deb (#2177)

--- .github/scripts/ubuntu/build_deb.sh
+++ .github/scripts/ubuntu/build_deb.sh
@@ -8,10 +8,10 @@ cargo install cargo-deb
 cd espanso
 
 echo "Building X11 deb package"
-cargo deb -p espanso -- --no-default-features --features "modulo vendored-tls"
+cargo deb --package espanso -- --no-default-features --features "modulo vendored-tls"
 
 echo "Building Wayland deb package"
-cargo deb -p espanso --variant wayland -- --no-default-features --features "modulo wayland vendored-tls"
+cargo deb --package espanso --variant wayland -- --no-default-features --features "modulo wayland vendored-tls"
 
 cd ..
 cp espanso/target/debian/espanso_*.deb espanso-debian-x11-amd64.deb
