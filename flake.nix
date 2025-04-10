761648e2ff08203a999c7b063b2b81d64ea24cfd
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-04-10 13:41:07 +0200

    :sparkles: add nix flake
    - add a flake that only takes nixpkgs as input
    - use `nixfmt-rfc-style` as nix formatter

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "A cross-platform Text Expander written in Rust";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      eachSystem =
+        with nixpkgs.lib;
+        f: foldAttrs mergeAttrs { } (map (s: mapAttrs (_: v: { ${s} = v; }) (f s)) supportedSystems);
+    in
+    eachSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        checks.espanso = self.packages.${system}.espanso.override { buildType = "debug"; };
+        formatter = pkgs.nixfmt-rfc-style;
+        packages = rec {
+          espanso = pkgs.callPackage ./nix/espanso.nix { };
+          espanso-wayland = pkgs.callPackage ./nix/espanso.nix {
+            waylandSupport = true;
+          };
+          default = espanso;
+        };
+        devShells =
+          let
+            commonRustFlagsEnv = "-C target-cpu=native";
+          in
+          {
+            default = pkgs.mkShell {
+              name = "espanso";
+              inputsFrom = [ self.checks.${system}.espanso ];
+              packages =
+                with pkgs;
+                [
+                  cargo-make
+                  rust-script
+                ]
+                ++ lib.optional (stdenv.isx86_64 && stdenv.isLinux) [ cargo-tarpaulin ];
+              shellHook = ''
+                export RUST_BACKTRACE="1"
+                export RUSTFLAGS="''${RUSTFLAGS:-""} ${commonRustFlagsEnv}";
+              '';
+            };
+          };
+      }
+    );
+}
