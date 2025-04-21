2a943329220e4bdae071316601657ce227e1122c
Auca Maillot <aucacoyan@gmail.com>
2025-04-21 18:25:24 -0300

    :memo: add docs, fix biome config and ignore hpp from formatting

--- scripts/format_clang.rs
+++ scripts/format_clang.rs
@@ -8,27 +8,26 @@ use std::process::Command;
 
 fn main() {
     for dir in [
-  "espanso",
-  "espanso-detect",
-  "espanso-ui",
-  "espanso-inject",
-  "espanso-ipc",
-  "espanso-config",
-  "espanso-match",
-  "espanso-clipboard",
-  "espanso-render",
-  "espanso-info",
-  "espanso-modulo",
-  "espanso-mac-utils",
-  "espanso-kvs",
-  "espanso-engine",
-  "espanso-package",
-  ]
-      {
-        for ext in ["c", "h", "cc", "hh", "cpp", "hpp"]
-        {
-              
-            for entry in glob(format!("{}/**/*.{}", dir, ext).as_str()).expect("Failed to read the glob pattern")
+        "espanso",
+        "espanso-detect",
+        "espanso-ui",
+        "espanso-inject",
+        "espanso-ipc",
+        "espanso-config",
+        "espanso-match",
+        "espanso-clipboard",
+        "espanso-render",
+        "espanso-info",
+        "espanso-modulo",
+        "espanso-mac-utils",
+        "espanso-kvs",
+        "espanso-engine",
+        "espanso-package",
+    ] {
+        for ext in ["c", "h", "cc", "hh", "cpp"] {
+            // ignore
+            for entry in glob(format!("{}/**/*.{}", dir, ext).as_str())
+                .expect("Failed to read the glob pattern")
             {
                 match entry {
                     Ok(path) => println!("{}", String::from_utf8(Command::new("clang-format")
