d8ce53835a9afab4f424165cbd8c5058c44bda06
Nathan Henrie <nate@n8henrie.com>
2025-04-29 15:25:54 -0600

    Set bash safemode
    Wrap in a function to allow for local variables
    
    pushd / popd instead of cd
    
    sort feature names

--- .github/scripts/ubuntu/build_deb.sh
+++ .github/scripts/ubuntu/build_deb.sh
@@ -1,24 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -Eeuf -o pipefail
 
-echo "Installing cargo-deb"
-cargo install cargo-deb
+main() {
+  echo "Installing cargo-deb"
+  cargo install cargo-deb
 
-cd espanso
+  pushd espanso
 
-echo "Building X11 deb package"
-cargo deb --package espanso -- --no-default-features --features "modulo vendored-tls"
+  echo "Building X11 deb package"
+  cargo deb --package espanso -- --no-default-features --features modulo,vendored-tls
 
-echo "Building Wayland deb package"
-cargo deb --package espanso --variant wayland -- --no-default-features --features "modulo wayland vendored-tls"
+  echo "Building Wayland deb package"
+  cargo deb --package espanso --variant wayland -- --no-default-features --features modulo,vendored-tls,wayland
+  cp espanso/target/debian/espanso_*.deb espanso-debian-x11-amd64.deb
 
-cd ..
-cp espanso/target/debian/espanso_*.deb espanso-debian-x11-amd64.deb
-sha256sum espanso-debian-x11-amd64.deb > espanso-debian-x11-amd64-sha256.txt
-cp espanso/target/debian/espanso-wayland*.deb espanso-debian-wayland-amd64.deb
-sha256sum espanso-debian-wayland-amd64.deb > espanso-debian-wayland-amd64-sha256.txt
-ls -la
+  popd
 
-echo "Copying to mounted volume"
-cp espanso-debian-* /shared
+  sha256sum espanso-debian-x11-amd64.deb > espanso-debian-x11-amd64-sha256.txt
+  cp espanso/target/debian/espanso-wayland*.deb espanso-debian-wayland-amd64.deb
+  sha256sum espanso-debian-wayland-amd64.deb > espanso-debian-wayland-amd64-sha256.txt
+
+  echo "Copying to mounted volume"
+  cp espanso-debian-* /shared
+}
+main "$@"
