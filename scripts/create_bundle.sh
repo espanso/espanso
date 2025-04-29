becf2e075a22a0acdf63d0cdbd814391224bac7c
Nathan Henrie <nate@n8henrie.com>
2025-04-29 15:38:55 -0600

    Remove EXEC_PATH from macos

--- scripts/create_bundle.sh
+++ scripts/create_bundle.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+# Creates an app bundle for MacOS
+#
+# Optionally accepts a path to an espanso executable as the first argument: if
+# not provided, by default it expects to find release binaries for both
+# x86_64-darwin and aarch64-darwin and package these in a universal binary for
+# the app bundle
+
 set -Eeuf -o pipefail
 
+readonly TARGET_DIR=target/mac/Espanso.app
+
 main() {
-  TARGET_DIR=target/mac/Espanso.app
+  # Pass in the binary to bundle as "$1"; default to universal
+  local espanso_bin=${1:-universal}
 
   rm -rf -- "${TARGET_DIR}"
 
@@ -19,8 +29,13 @@ main() {
 
   cp -f espanso/src/res/macos/icon.icns "${TARGET_DIR}"/Contents/Resources/icon.icns
 
+  if [[ "${espanso_bin}" != universal ]]; then
+    cp "${espanso_bin}" "${TARGET_DIR}/Contents/MacOS/espanso"
+    return
+  fi
+
   lipo -create \
     -output "${TARGET_DIR}/Contents/MacOS/espanso" \
-    target/x86_64-apple-darwin/release/espanso target/aarch64-apple-darwin/release/espanso
+    target/aarch64-apple-darwin/release/espanso target/x86_64-apple-darwin/release/espanso
 }
 main "$@"
