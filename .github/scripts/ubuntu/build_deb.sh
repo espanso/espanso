5b802cbee1b3548d10c4afa7ae577d22ad185b58
Nathan Henrie <nate@n8henrie.com>
2025-04-30 11:45:25 -0600

    find after popd

--- .github/scripts/ubuntu/build_deb.sh
+++ .github/scripts/ubuntu/build_deb.sh
@@ -13,10 +13,10 @@ main() {
 
   echo "Building Wayland deb package"
   cargo deb --package espanso --variant wayland -- --no-default-features --features modulo,vendored-tls,wayland
-  find ./espanso/target/debian -name 'espanso_*.deb' -exec cp {} espanso-debian-x11-amd64.deb \; -quit
 
   popd
 
+  find ./espanso/target/debian -name 'espanso_*.deb' -exec cp {} espanso-debian-x11-amd64.deb \; -quit
   sha256sum espanso-debian-x11-amd64.deb > espanso-debian-x11-amd64-sha256.txt
   find ./espanso/target/debian -name 'espanso-wayland*.deb' -exec cp {} espanso-debian-wayland-amd64.deb \; -quit
   sha256sum espanso-debian-wayland-amd64.deb > espanso-debian-wayland-amd64-sha256.txt
