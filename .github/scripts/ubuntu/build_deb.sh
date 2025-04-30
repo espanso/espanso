f1196d0ccc54edcbf704a0735966ceef1d7dea77
Nathan Henrie <nate@n8henrie.com>
2025-04-30 11:45:37 -0600

    Logging, set debug mode during development

--- .github/scripts/ubuntu/build_deb.sh
+++ .github/scripts/ubuntu/build_deb.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
 
 set -Eeuf -o pipefail
+# TODO
+set -x
+
+log() {
+  printf '%s\n' "$*" >&2
+}
 
 main() {
-  echo "Installing cargo-deb"
+  log "Installing cargo-deb"
   cargo install cargo-deb
 
   pushd espanso
 
-  echo "Building X11 deb package"
+  log "Building X11 deb package"
   cargo deb --package espanso -- --no-default-features --features modulo,vendored-tls
 
-  echo "Building Wayland deb package"
+  log "Building Wayland deb package"
   cargo deb --package espanso --variant wayland -- --no-default-features --features modulo,vendored-tls,wayland
 
   popd
 
   find ./espanso/target/debian -name 'espanso_*.deb' -exec cp {} espanso-debian-x11-amd64.deb \; -quit
   sha256sum espanso-debian-x11-amd64.deb > espanso-debian-x11-amd64-sha256.txt
+
   find ./espanso/target/debian -name 'espanso-wayland*.deb' -exec cp {} espanso-debian-wayland-amd64.deb \; -quit
   sha256sum espanso-debian-wayland-amd64.deb > espanso-debian-wayland-amd64-sha256.txt
 
-  echo "Copying to mounted volume"
+  log "Copying to mounted volume"
   find . -maxdepth 1 -name 'espanso-debian-*' -exec cp -t /shared {} +
 }
 main "$@"
