3ebc88e519d66d94b5cc47ec590fe030a986ca4d
Nathan Henrie <nate@n8henrie.com>
2025-05-01 09:36:49 -0600

    Revert "deprecate fedora"
    This reverts commit 7eba5a5f32f178056695bf6489831d257f77eae9.

---
+++ .github/scripts/fedora/build_rpm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing cargo-generate-rpm"
+cargo install cargo-generate-rpm --version 0.14.0
+
+cd espanso
+
+echo "Building Wayland"
+cargo make --profile release --env NO_X11=true build-binary
+
+echo "Building Wayland RPM package"
+cargo generate-rpm -p espanso
+
+cd ..
+cp espanso/target/generate-rpm/*.rpm espanso-fedora-wayland-amd64.rpm
+sha256sum espanso-fedora-wayland-amd64.rpm > espanso-fedora-wayland-amd64-sha256.txt
+ls -la
+
+echo "Copying to mounted volume"
+cp espanso-fedora-* /shared
