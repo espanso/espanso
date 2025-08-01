6923edcef98bf3154955f6f2642af35a87d16e46
Auca Coyan <aucacoyan@gmail.com>
2025-08-01 20:41:19 +0000

    :arrow_up: upgrade winrt and notify-rs (#2412)
    * :arrow_up: upgrade winrt and notify-rs
    
    replaced winrt-notification for tauri-winrt-notification and upgraded the version of notify-rust
    
    * :bug: fix cargo.lock

--- espanso-ui/src/win32/notification.rs
+++ espanso-ui/src/win32/notification.rs
@@ -30,7 +30,7 @@ use log::{error, warn};
 use std::os::windows::process::CommandExt;
 use std::process::Command;
 use std::sync::LazyLock;
-use winrt_notification::{IconCrop, Toast};
+use tauri_winrt_notification::{IconCrop, Toast};
 
 const ESPANSO_APP_USER_MODEL_ID: &str = "{5E3B6C0F-1A4D-45C4-8872-D8174702101A}";
 
