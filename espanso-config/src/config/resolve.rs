76142c7947b0369a6e21e4556d27d966020ed02b
smeech <stephen@smeech.plus.com>
2025-08-12 18:05:17 +0100

    Revert "Update resolve.rs"
    This reverts commit 9a474e6ffcb523dc8ac4026bf2061ca165ee594f.

--- espanso-config/src/config/resolve.rs
+++ espanso-config/src/config/resolve.rs
@@ -254,7 +254,6 @@ impl Config for ResolvedConfig {
         "\t".to_string(),
         "\n".to_string(),
         "\x0c".to_string(), // Form Feed
-        "\xa0".to_string(), // Non-breaking space
       ]
     })
   }
