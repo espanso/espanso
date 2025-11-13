espanso/espanso

commit 7bae4770073eac000c7711df8af8925509cefd1c

fix `clippy` lints (#2171)
* :recycle: put `$auto` in cargo-deb dependencies

* :rotating_light: fix clippy lints

* :rotating_light: fix more lints

* :rotating_light: even more fixes

* :rotating_light: try to fix the lazy_static lint

* :rotating_light: final lints

* Revert ":recycle: put `$auto` in cargo-deb dependencies"

This reverts commit 2cc6ca3d3efc790d93f0ae50c3fcb402c043d21d.

espanso/src/cli/worker/ui/welcome.rs
@@ -23,7 +23,10 @@ pub fn show_welcome_screen() {
   let mut command = std::process::Command::new(espanso_exe_path.to_string_lossy().to_string());
   command.args(["modulo", "welcome"]);
 
-  command.spawn().expect("unable to show welcome screen");
+  let _ = command
+    .spawn()
+    .expect("unable to show welcome screen")
+    .wait();
 }
 
 #[cfg(not(feature = "modulo"))]

espanso-clipboard/src/win32/mod.rs
@@ -30,8 +30,8 @@ use widestring::{U16CStr, U16CString};
 pub struct Win32Clipboard {}
 
 impl Win32Clipboard {
-  pub fn new() -> Result<Self> {
-    Ok(Self {})
+  pub fn new() -> Self {
+    Self {}
   }
 }
 

espanso-render/src/renderer/mod.rs
@@ -53,7 +53,7 @@ impl<'a> DefaultRenderer<'a> {
   }
 }
 
-impl<'a> Renderer for DefaultRenderer<'a> {
+impl Renderer for DefaultRenderer<'_> {
   fn render(
     &self,
     template: &Template,

Cargo.toml
@@ -55,7 +55,7 @@ dirs = "3.0.1"
 enum-as-inner = "0.6.0"
 fs_extra = "1.3.0"
 glob = "0.3.0"
-lazy_static = "1.4.0"
+lazy_static = "1.5.0"
 lazycell = "1.3.0"
 log = "0.4.14"
 regex = "1.5.5"

espanso-clipboard/src/lib.rs
@@ -73,7 +73,7 @@ impl Default for ClipboardOptions {
 #[cfg(target_os = "windows")]
 pub fn get_clipboard(_: ClipboardOptions) -> Result<Box<dyn Clipboard>> {
   info!("using Win32Clipboard");
-  Ok(Box::new(win32::Win32Clipboard::new()?))
+  Ok(Box::new(win32::Win32Clipboard::new()))
 }
 
 #[cfg(target_os = "macos")]

espanso-engine/src/dispatch/default.rs
@@ -73,7 +73,7 @@ impl<'a> DefaultDispatcher<'a> {
   }
 }
 
-impl<'a> Dispatcher for DefaultDispatcher<'a> {
+impl Dispatcher for DefaultDispatcher<'_> {
   fn dispatch(&self, event: Event) {
     for executor in &self.executors {
       if executor.execute(&event) {

espanso-engine/src/dispatch/executor/context_menu.rs
@@ -36,7 +36,7 @@ impl<'a> ContextMenuExecutor<'a> {
   }
 }
 
-impl<'a> Executor for ContextMenuExecutor<'a> {
+impl Executor for ContextMenuExecutor<'_> {
   fn execute(&self, event: &Event) -> bool {
     if let EventType::ShowContextMenu(context_menu_event) = &event.etype {
       if let Err(error) = self.handler.show_context_menu(&context_menu_event.items) {

espanso-engine/src/dispatch/executor/html_inject.rs
@@ -39,7 +39,7 @@ impl<'a> HtmlInjectExecutor<'a> {
   }
 }
 
-impl<'a> Executor for HtmlInjectExecutor<'a> {
+impl Executor for HtmlInjectExecutor<'_> {
   fn execute(&self, event: &Event) -> bool {
     if let EventType::HtmlInject(inject_event) = &event.etype {
       // Render the text fallback for those applications that don't support HTML clipboard

commit 598dd1017ee5b88962e14fffe3766bd2eb0dd6ae

Restore espansion in the welcome screen
The addition of the `wait` in 7bae4770073eac000c7711df8af8925509cefd1c broke expansion in the welcome screen.

Fixes https://github.com/espanso/espanso/issues/2571

espanso/src/cli/worker/engine/mod.rs
@@ -282,6 +282,7 @@ pub fn initialize_and_spawn(
                 }
             }
 
+            let mut welcome_handle = None;
             match start_reason.as_deref() {
                 Some(flag) if flag == WORKER_START_REASON_CONFIG_CHANGED => {
                     notification_manager.notify_config_reloaded(false);
@@ -296,7 +297,7 @@ pub fn initialize_and_spawn(
                     notification_manager.notify_start();
 
                     if !preferences.has_displayed_welcome() {
-                        super::ui::welcome::show_welcome_screen();
+                        welcome_handle = super::ui::welcome::show_welcome_screen();
                         preferences.set_has_displayed_welcome(true);
                     }
                 }
@@ -304,6 +305,9 @@ pub fn initialize_and_spawn(
 
             let mut engine = espanso_engine::Engine::new(&funnel, &mut processor, &dispatcher);
             let exit_mode = engine.run();
+            if let Some(mut handle) = welcome_handle {
+                handle.wait().expect("welcome screen died");
+            };
 
             info!("engine eventloop has terminated, propagating exit event...");
             ui_remote.exit();

espanso/src/cli/worker/ui/welcome.rs
@@ -16,20 +16,18 @@
  * You should have received a copy of the GNU General Public License
  * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
  */
+use std::process::Child;
 
 #[cfg(feature = "modulo")]
-pub fn show_welcome_screen() {
+pub fn show_welcome_screen() -> Option<Child> {
     let espanso_exe_path = std::env::current_exe().expect("unable to determine executable path");
     let mut command = std::process::Command::new(espanso_exe_path.to_string_lossy().to_string());
     command.args(["modulo", "welcome"]);
 
-    let _ = command
-        .spawn()
-        .expect("unable to show welcome screen")
-        .wait();
+    Some(command.spawn().expect("unable to show welcome screen"))
 }
 
 #[cfg(not(feature = "modulo"))]
-pub fn show_welcome_screen() {
-    // NOOP
+pub fn show_welcome_screen() -> Option<Child> {
+    None
 }