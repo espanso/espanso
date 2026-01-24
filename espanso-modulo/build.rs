6c3fa6f92e6a16b60ef0f55708198be02bc36da0
Stephan Boldt <github@sboldt.de>
2026-01-24 02:33:26 +0100

    build: touch build.rs to force C++ recompilation
    When C++ files are modified, Cargo doesn't automatically detect the changes.
    Modifying build.rs forces Cargo to re-run the build script and recompile
    all C++ files including the clipboard fix in export_dialog.cpp.

--- espanso-modulo/build.rs
+++ espanso-modulo/build.rs
@@ -2,7 +2,7 @@
  * This file is part of espanso.
  *
  * Copyright (C) 2021 Federico Terzi
- *
+ * 
  * espanso is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
